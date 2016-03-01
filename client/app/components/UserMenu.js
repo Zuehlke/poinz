67545aed7e2f102721f31b4e7bf1a9f8370c8952
Sergio Trentini <xeronimus@gmail.com>
2016-03-01 21:45:08 +0100

    fix css classes on "leave room button"

--- client/app/components/UserMenu.js
+++ client/app/components/UserMenu.js
@@ -49,9 +49,9 @@ const UserMenu = ({user, setUsername, leaveRoom, toggleVisitor, userMenuShown}) 
         <ActionLog />
       </div>
 
-      <button className='leave-room-button pure-button pure-button-primary" type="button' onClick={leaveRoom}>
+      <button className='leave-room-button pure-button pure-button-primary' type='button' onClick={leaveRoom}>
         Leave Room
-        <i className='fa fa-sign-out'></i>
+        <i className='fa fa-sign-out button-icon-right'></i>
       </button>
     </div>
   );

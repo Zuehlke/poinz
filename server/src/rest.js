9649e3adc827b31771a13f91fb3d117e23bb8e2f
Sergio Trentini <xeronimus@gmail.com>
2016-09-09 08:18:10 +0200

    REST endpoint for translations

--- server/src/rest.js
+++ server/src/rest.js
@@ -18,17 +18,27 @@ const
  */
 function init(app) {
   var restRouter = express.Router();
-  restRouter.get('/status', handleStatusRequest);
+  restRouter.get('/status', (req, res) => buildStatusObject().then(status => sendResponseObject(res, status)));
+  restRouter.get('/translations', (req, res) => getTranslations(req.query.lang).then(translations => sendResponseObject(res, translations)));
   app.use('/api', restRouter);
 }
 
-function handleStatusRequest(req, res) {
-  buildStatusObject().then(status => {
-    res.contentType('application/json');
-    res.json(status);
-  });
+/**
+ * sets response content type to application/json and sends given object as json body
+ *
+ * @param res
+ * @param object
+ */
+function sendResponseObject(res, object) {
+  res.contentType('application/json');
+  res.json(object);
 }
 
+/**
+ * builds the application status object. contains information about all rooms, connected users, etc.
+ *
+ * @returns {Promise.<Immutable.List>}
+ */
 function buildStatusObject() {
   return roomsStore
     .getAllRooms()
@@ -51,6 +61,16 @@ function buildStatusObject() {
         uptime: Math.floor(process.uptime())
       };
     });
+}
+
+
+/**
+ * return translations for given language
+ *
+ * @param {string} language a two char language code (e.g. "en" or "de")
+ */
+function getTranslations(language) {
+  debugger;
 }
 
 module.exports = {init};

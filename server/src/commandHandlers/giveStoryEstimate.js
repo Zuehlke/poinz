3d935f863dd4344b87bfb6a4514a73ea5c9cd3b3
sergio <xeronimus@gmail.com>
2016-11-23 15:14:37 +0100

    extract user estimation check into separate function

--- server/src/commandHandlers/giveStoryEstimate.js
+++ server/src/commandHandlers/giveStoryEstimate.js
@@ -31,21 +31,38 @@ const giveStoryEstimateCommandHandler = {
     // this could be improved in the future.. (e.g. not send value with "storyEstimateGiven" -> but send all values later with "revealed" )
     room.applyEvent('storyEstimateGiven', command.payload);
 
-    // now check if every user in the room (that is not marked as visitor and is not disconnected)  did estimate the current story
-    const userIdsThatHaveEstimateGiven = room.getIn(['stories', command.payload.storyId, 'estimations']).keySeq().toJS();
-    userIdsThatHaveEstimateGiven.push(command.payload.userId);
-    const allUserIdsThatCanEstimate = room.get('users')
-      .filter(usr => !usr.get('visitor'))
-      .filter(usr => !usr.get('disconnected'))
-      .keySeq().toJS();
 
-    if (userIdsThatHaveEstimateGiven.length <= allUserIdsThatCanEstimate.length && _.isEqual(userIdsThatHaveEstimateGiven.sort(), allUserIdsThatCanEstimate.sort())) {
+    if (allValidUsersEstimated(room, command)) {
+
       room.applyEvent('revealed', {
         storyId: command.payload.storyId,
         manually: false
       });
+
+      room.applyEvent('storyPointsSet', {
+        storyId: command.payload.storyId,
+        value: command.payload.value
+      })
     }
   }
 };
+
+/**
+ * checks if every user in the room (that is not marked as visitor and is not disconnected)  did estimate the current story
+ * @param room
+ * @param command
+ * @returns {boolean}
+ */
+function allValidUsersEstimated(room, command) {
+  const userIdsThatHaveEstimateGiven = room.getIn(['stories', command.payload.storyId, 'estimations']).keySeq().toJS();
+  userIdsThatHaveEstimateGiven.push(command.payload.userId);
+  const allUserIdsThatCanEstimate = room.get('users')
+    .filter(usr => !usr.get('visitor'))
+    .filter(usr => !usr.get('disconnected'))
+    .keySeq().toJS();
+
+  return (userIdsThatHaveEstimateGiven.length <= allUserIdsThatCanEstimate.length
+  && _.isEqual(userIdsThatHaveEstimateGiven.sort(), allUserIdsThatCanEstimate.sort()));
+}
 
 export default giveStoryEstimateCommandHandler;

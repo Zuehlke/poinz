3d74ed363f0595941ff58ead97f4e5c2bd40eb67
xeronimus@gmail.com <sTrentwow890>
2020-05-30 09:31:55 +0200

    test cases for time and date formatting

--- client/test/unit/timeUtilTest.js
+++ client/test/unit/timeUtilTest.js
@@ -1,4 +1,4 @@
-import {secondsToDaysHoursMinutes} from '../../app/services/timeUtil';
+import {formatDateTime, formatTime, secondsToDaysHoursMinutes} from '../../app/services/timeUtil';
 
 test('#secondsToDaysHoursMinutes 1 day 1 hour 1 minute', () => {
   const result = secondsToDaysHoursMinutes(60 * 60 * 24 + 60 * 60 + 60);
@@ -9,3 +9,24 @@ test('#secondsToDaysHoursMinutes 2 days 2 hours 2 minutes', () => {
   const result = secondsToDaysHoursMinutes(2 * 60 * 60 * 24 + 2 * 60 * 60 + 2 * 60);
   expect(result).toEqual('2 days 2 hours 2 minutes');
 });
+
+test('#formatDateTime ms', () => {
+  const result = formatDateTime(1234567890 * 1000);
+  expect(result).toEqual('14.02.2009 00:31');
+})
+
+test('#formatDateTime seconds instead of milliseconds', () => {
+  const result = formatDateTime(1234567890);
+  expect(result).toEqual('15.01.1970 07:56'); // still works, but gives warning
+})
+
+test('#formatDateTime date', () => {
+  const result = formatDateTime(new Date(1234567890 * 1000));
+  expect(result).toEqual('14.02.2009 00:31');
+})
+
+test('#formatDate', () => {
+  const result = formatTime(1234567890 * 1000);
+  expect(result).toEqual('00:31');
+})
+

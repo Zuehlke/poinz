ef2a79478151f56541ad3384508f2d9afe586faf
xeronimus@gmail.com <sTrentwow890>
2020-06-07 10:49:09 +0200

    # alternative solution to docu generation:  parse source files for information

---
+++ server/docu/docuGenerator.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const fs = require('fs');
+
+const chalk = require('chalk');
+
+const gatherData = require('./gatherData');
+const renderDocu = require('./renderDocu');
+
+const cmdHandlersDirPath = path.join(__dirname, '../src/commandHandlers');
+const validationSchemasDirPath = path.join(__dirname, '../resources/validationSchemas');
+const evtHandlersDirPath = path.join(__dirname, '../src/eventHandlers');
+
+generate().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+async function generate() {
+  console.log(chalk.blue.bold('Generating commands and events documentation for Poinz:'));
+  console.log(
+    `  Expecting commandHandlers in "${cmdHandlersDirPath}"\n  Expecting validationSchemas in "${validationSchemasDirPath}"\n  Expecting eventHandlers in "${evtHandlersDirPath}"`
+  );
+
+  const data = await gatherData(cmdHandlersDirPath, validationSchemasDirPath, evtHandlersDirPath);
+
+  const markdownString = renderDocu(data);
+  await fs.promises.writeFile(
+    path.join(__dirname, './commandAndEventDocu.md'),
+    markdownString,
+    'utf-8'
+  );
+}

7b9f9b2a0e87b8b6dbe7450597de4f95c0ca4c08
xeronimus@gmail.com <sTrentwow890>
2020-08-04 08:31:07 +0200

    #58 switch env variable name for mongodb connection
    - mlab will no longer be available on heroku
    - switch to mongodb atlas

--- server/src/settings.js
+++ server/src/settings.js
@@ -21,11 +21,13 @@ const settings = {
     }
   },
 
-  // if environment variable "MONGODB_URI" is set (like in heroku prod deployment), persistent store is used.
+  // if environment variable "ATLAS_DB_URI" is set (in heroku deployment), persistent store is used.
   // if env variable is not set, "persistentStore" is false and in-memory store is used.
-  persistentStore: process.env.MONGODB_URI
+
+  // as of august 2020, switched from mLab to mongoDB Atlas
+  persistentStore: process.env.ATLAS_DB_URI
     ? {
-        connectionURI: process.env.MONGODB_URI
+        connectionURI: process.env.ATLAS_DB_URI
       }
     : false
 };

29ac0bdea29e32fb75ad33c1715df7f9e46a1b9e
xeronimus@gmail.com <sTrentwow890>
2021-09-17 08:26:02 +0200

    #58 update npm dependencies. remove bluebird

--- build/grabUpdatedFontelloZip.js
+++ build/grabUpdatedFontelloZip.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const Promise = require('bluebird');
+const util = require('util');
 const fs = require('fs-extra');
 const {exec} = require('child_process');
 const del = require('del');

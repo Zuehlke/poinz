b1f0e6a2b54d4bf5f3db35f7b3d35131a11c2c96
xeronimus@gmail.com <sTrentwow890>
2022-02-04 19:27:47 +0100

    #58 remove node-polyglot since we are not using pluralization - replace with simple dictionary lookup

--- client/test/unit/l10nTest.js
+++ client/test/unit/l10nTest.js
@@ -1,19 +1,33 @@
 /* eslint react/prop-types: 0 */
 import React, {useContext, useEffect} from 'react';
-import TestRenderer from 'react-test-renderer';
+import {create, act} from 'react-test-renderer';
 
 import {L10nContext, WithL10n} from '../../app/services/l10n';
 
+beforeEach(() => {
+  const InnerComponent = () => {
+    const {setLanguage} = useContext(L10nContext);
+    useEffect(() => setLanguage('en'), []);
+    return null;
+  };
+
+  act(() => {
+    create(
+      <WithL10n>
+        <InnerComponent />
+      </WithL10n>
+    );
+  });
+});
+
 test('WithL10n can mount', () => {
-  const testRenderer = TestRenderer.create(
+  const tr = create(
     <WithL10n>
       <div className="inner" />
     </WithL10n>
   );
 
-  const testInstance = testRenderer.root;
-
-  expect(testInstance.findByProps({className: 'inner'})).toBeDefined();
+  expect(tr.root.findByProps({className: 'inner'})).toBeDefined();
 });
 
 test('WithL10n : Inner component can access "t" function and translate default language', () => {
@@ -22,16 +36,91 @@ test('WithL10n : Inner component can access "t" function and translate default l
     return <div className="inner">{t('backlog')}</div>;
   };
 
-  let testRenderer;
-  TestRenderer.act(() => {
-    testRenderer = TestRenderer.create(
+  let tr;
+  act(() => {
+    tr = create(
       <WithL10n>
         <InnerComponent />
       </WithL10n>
     );
   });
 
-  expect(testRenderer.root.findByProps({className: 'inner'}).children).toEqual(['Backlog']);
+  expect(tr.root.findByProps({className: 'inner'}).children).toEqual(['Backlog']);
+});
+
+test('WithL10n : can translate with interpolations', () => {
+  const InnerComponent = () => {
+    const {t} = useContext(L10nContext);
+    return <div className="inner">{t('joinWithRoomName', {room: 'mein-super-raum'})}</div>;
+  };
+
+  let tr;
+  act(() => {
+    tr = create(
+      <WithL10n>
+        <InnerComponent />
+      </WithL10n>
+    );
+  });
+
+  expect(tr.root.findByProps({className: 'inner'}).children).toEqual([
+    'Join room "mein-super-raum"'
+  ]);
+});
+
+test('WithL10n : can translate with interpolations (empty string)', () => {
+  const InnerComponent = () => {
+    const {t, setLanguage} = useContext(L10nContext);
+    useEffect(() => setLanguage('de'), []);
+    return <div className="inner">{t('joinWithRoomName', {room: ''})}</div>;
+  };
+
+  let tr;
+  act(() => {
+    tr = create(
+      <WithL10n>
+        <InnerComponent />
+      </WithL10n>
+    );
+  });
+
+  expect(tr.root.findByProps({className: 'inner'}).children).toEqual(['Raum "" beitreten']);
+});
+
+test('WithL10n : can translate with interpolations (values not passed)', () => {
+  const InnerComponent = () => {
+    const {t} = useContext(L10nContext);
+    return <div className="inner">{t('joinWithRoomName', {thereIsNoRoom: 'property'})}</div>;
+  };
+
+  let tr;
+  act(() => {
+    tr = create(
+      <WithL10n>
+        <InnerComponent />
+      </WithL10n>
+    );
+  });
+
+  expect(tr.root.findByProps({className: 'inner'}).children).toEqual(['Join room "%{room}"']);
+});
+
+test('WithL10n : handles missing translation for given key', () => {
+  const InnerComponent = () => {
+    const {t} = useContext(L10nContext);
+    return <div className="inner">{t('thisKeyDoesNotExist')}</div>;
+  };
+
+  let tr;
+  act(() => {
+    tr = create(
+      <WithL10n>
+        <InnerComponent />
+      </WithL10n>
+    );
+  });
+
+  expect(tr.root.findByProps({className: 'inner'}).children).toEqual(['!!!thisKeyDoesNotExist!!!']);
 });
 
 test('WithL10n : setLanguage', () => {
@@ -45,23 +134,23 @@ test('WithL10n : setLanguage', () => {
     return <div className="inner">{t('room')}</div>;
   };
 
-  let testRenderer;
-  TestRenderer.act(() => {
-    testRenderer = TestRenderer.create(
+  let tr;
+  act(() => {
+    tr = create(
       <WithL10n>
         <InnerComponent />
       </WithL10n>
     );
   });
-  expect(testRenderer.root.findByProps({className: 'inner'}).children).toEqual(['Room']);
+  expect(tr.root.findByProps({className: 'inner'}).children).toEqual(['Room']);
 
   // update with customLanguage property set to "de"
-  TestRenderer.act(() => {
-    testRenderer.update(
+  act(() => {
+    tr.update(
       <WithL10n>
         <InnerComponent customLanguage="de" />
       </WithL10n>
     );
   });
-  expect(testRenderer.root.findByProps({className: 'inner'}).children).toEqual(['Raum']);
+  expect(tr.root.findByProps({className: 'inner'}).children).toEqual(['Raum']);
 });

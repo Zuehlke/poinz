067903c58e88cbd71990b6230819ad9c2d4c4a4d
RexShum <rex.shum@zuhlke.com>
2025-01-21 20:23:52 +0800

    Remove babel script removing test id in production

--- client/viteConfig/babelConfig.ts
+++ client/viteConfig/babelConfig.ts
@@ -1,13 +1,6 @@
-const plugins = [
-  '@quickbaseoss/babel-plugin-styled-components-css-namespace',
-  'babel-plugin-styled-components'
-];
-
 export const babel = {
-  plugins,
-  env: {
-    production: {
-      plugins: ['./viteConfig/removeDataTestIdAttributes.js', ...plugins]
-    }
-  }
+  plugins: [
+    '@quickbaseoss/babel-plugin-styled-components-css-namespace',
+    'babel-plugin-styled-components'
+  ]
 };

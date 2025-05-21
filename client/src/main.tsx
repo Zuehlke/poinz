ac307eca9d6c99238df1f7dc013a6d84ef6e246c
Mika Ruch <mika.ruch@zuehlke.com>
2025-05-21 15:51:40 +0200

    feat: add posthog

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,18 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import {PostHogProvider} from 'posthog-js/react';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <App />
+    </PostHogProvider>
   </StrictMode>
 );
--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PUBLIC_POSTHOG_KEY: string
+  readonly VITE_PUBLIC_POSTHOG_HOST: string
+  // more env variables can be declared here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
